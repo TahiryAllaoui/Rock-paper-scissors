44dca30cc182e23fe343e2a6429d4d60d36ad24a
TahiryAllaoui <tallaoui31@gmail.com>
2023-09-07 21:11:35 +0300

    Rules backend don

--- src/components/Rules.tsx
+++ src/components/Rules.tsx
@@ -1,14 +1,27 @@
 import '../style/Rules.scss';
 
 const Rules = () => {
+    const handleClick = () => {
+        let container = document.querySelector('.rules .container') as HTMLElement;
+        container.style.display = 'flex';
+        container.style.alignItems = 'center';
+        container.style.justifyContent = 'center';
+    };
+    const handleClose = () => {
+        let container = document.querySelector('.rules .container') as HTMLElement;
+        container.style.display = 'none';
+    };
+
+
+
     return (
         <div className="rules">
-            <div className="my-button">RULES</div>
+            <div className="my-button" onClick={handleClick}>RULES</div>
             <div className="container">
                 <div className="rules-container">
                     <div className="rules-header">
                         <h2>RULES</h2>
-                        <div className="close"></div>
+                        <div className="close" onClick={handleClose}></div>
                     </div>
                     <div className="rules-item"></div>
                 </div>
