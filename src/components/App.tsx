536fdbd1678f3c6d453ad65b64078e01b9cbcc42
TahiryAllaoui <tallaoui31@gmail.com>
2023-09-07 20:46:44 +0300

    Rules displayed

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,6 @@
 import '../style/App.scss'
 import Game from './Game'
+import Rules from './Rules'
 import Score from './Score'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className='app'>
       <Score />
       <Game />
+      <Rules />
     </div>
   )
 }
---
+++ src/components/Rules.tsx
@@ -0,0 +1,20 @@
+import '../style/Rules.scss';
+
+const Rules = () => {
+    return (
+        <div className="rules">
+            <div className="my-button">RULES</div>
+            <div className="container">
+                <div className="rules-container">
+                    <div className="rules-header">
+                        <h2>RULES</h2>
+                        <div className="close"></div>
+                    </div>
+                    <div className="rules-item"></div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Rules;
