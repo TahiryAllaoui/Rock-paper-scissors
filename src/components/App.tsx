b7ede079a0f3e24222c74d0c1b509b97799a112c
TahiryAllaoui <tallaoui31@gmail.com>
2023-09-07 19:02:07 +0300

    Scored-board displayed

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import '../style/App.scss'
+import Score from './Score'
+
+function App() {
+
+  return (
+    <div className='app'>
+      <Score />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Score.tsx
@@ -0,0 +1,17 @@
+import '../style/Score.scss';
+
+
+const Score = () => {
+    return (
+        <div className="score-board">
+            <h1>ROCK <br />PAPER <br />SCISSORS</h1>
+            <div className="score">
+                <h2>SCORE</h2>
+                <p>12</p>
+            </div>
+        </div>
+    );
+};
+
+export default Score;
+
