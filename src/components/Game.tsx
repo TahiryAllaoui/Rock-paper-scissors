2ff4a7acf3fbb77ca03e7e238c5e01e3b1100afc
TahiryAllaoui <tallaoui31@gmail.com>
2023-09-07 20:07:09 +0300

    Game layout done

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,18 +1,20 @@
 import '../style/Game.scss';
-import paper from '../assets/images/icon-paper.svg'
-import scissors from '../assets/images/icon-scissors.svg'
-import rock from '../assets/images/icon-rock.svg'
-
 
 const Game = () => {
     return (
         <div className="game">
             <div className="top-hand">
-                <div className="paper"></div>
-                <div className="scissors"></div>
+                <div className="paper">
+                    <div className="inner-paper"></div>
+                </div>
+                <div className="scissors">
+                    <div className="inner-scissors"></div>
+                </div>
             </div>
             <div className="bottom-hand">
-                <div className="rock"></div>
+                <div className="rock">
+                    <div className="inner-rock"></div>
+                </div>
             </div>
         </div>
     );
