38037b147289f1062da8e77c13f510322035eac3
TahiryAllaoui <tallaoui31@gmail.com>
2023-09-09 10:43:06 +0300

    Game basics and styling redonewith flex

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import '../style/Game.scss';
 
 interface moves {
@@ -23,7 +24,10 @@ const Game = () => {
 
     const npcMove: string[] = ['rock', 'paper', 'scissors'];
 
+    const [party, setParty] = useState(true);
+
     const handleClick = (hand: moves) => {
+        setParty(!party);
         let npcIndex: number = Math.floor(Math.random() * 3);
         console.log('You choosed ' + hand.name + ' and the house picked ' + npcMove[npcIndex]);
         if (hand.name == npcMove[npcIndex]) {
@@ -40,19 +44,25 @@ const Game = () => {
 
     return (
         <div className="game">
-            <div className="top-hand">
-                <div className="paper" onClick={() => handleClick(hand[1])}>
-                    <div className="inner-paper"></div>
+            {party ? <div className="hand-selection">
+                <div className="top-hand">
+                    <div className="paper" onClick={() => handleClick(hand[1])}>
+                        <div className="inner-paper"></div>
+                    </div>
+                    <div className="scissors" onClick={() => handleClick(hand[2])}>
+                        <div className="inner-scissors"></div>
+                    </div>
                 </div>
-                <div className="scissors" onClick={() => handleClick(hand[2])}>
-                    <div className="inner-scissors"></div>
+                <div className="bottom-hand">
+                    <div className="rock" onClick={() => handleClick(hand[0])}>
+                        <div className="inner-rock"></div>
+                    </div>
                 </div>
-            </div>
-            <div className="bottom-hand">
-                <div className="rock" onClick={() => handleClick(hand[0])}>
-                    <div className="inner-rock"></div>
+            </div> :
+                <div className="summary">
+
                 </div>
-            </div>
+            }
         </div>
     );
 };
