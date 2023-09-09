10a25d3c87d258893e83625a71782734afa60238
TahiryAllaoui <tallaoui31@gmail.com>
2023-09-09 21:23:56 +0300

    Finished mobile version

--- src/components/Rules.tsx
+++ src/components/Rules.tsx
@@ -3,23 +3,36 @@ import '../style/Rules.scss';
 const Rules = () => {
     const handleClick = () => {
         let container = document.querySelector('.rules .container') as HTMLElement;
+        let body = document.querySelector('body') as HTMLElement;
         container.style.display = 'flex';
         container.style.alignItems = 'center';
         container.style.justifyContent = 'center';
         container.style.opacity = '0';
         container.style.zIndex = '1';
+        container.style.transform = 'scaleY(1)';
 
         setTimeout(() => {
             container.style.opacity = '1'
         }, 100);
+
+        if (body.clientWidth <= 428) {
+            container.style.transform = 'scaleY(1)';
+        }
     };
     const handleClose = () => {
         let container = document.querySelector('.rules .container') as HTMLElement;
+        let body = document.querySelector('body') as HTMLElement;
+        container.style.display = 'flex';
         container.style.opacity = '1';
         setTimeout(() => {
             container.style.opacity = '0';
             container.style.zIndex = '-2';
         }, 200);
+
+
+        if (body.clientWidth <= 428) {
+            container.style.transform = 'scaleY(0)';
+        }
     };
 
 
