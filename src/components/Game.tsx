f3ab3a6ab1e197015ca72fcb3694da5474a7babe
TahiryAllaoui <tallaoui31@gmail.com>
2023-09-09 12:31:08 +0300

    Game summary placeholder

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,50 +1,103 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import '../style/Game.scss';
+import triangle from '../assets/images/bg-triangle.svg';
+import rockIcon from '../assets/images/icon-rock.svg';
+import paperIcon from '../assets/images/icon-paper.svg';
+import scissorsIcon from '../assets/images/icon-scissors.svg';
 
 interface moves {
     name: string;
     beat: string;
     lose: string;
+    icon: string;
+    bg: string;
+    borderBottom: string;
 }
 
 const Game = () => {
     const hand: moves[] = [{
         name: 'rock',
         beat: 'scissors',
-        lose: 'paper'
+        lose: 'paper',
+        icon: rockIcon,
+        bg: 'radial-gradient(hsl(349, 71%, 52%), hsl(349, 70%, 56%))',
+        borderBottom: 'hsl(349, 80%, 46%) 10px solid'
     }, {
         name: 'paper',
         beat: 'rock',
-        lose: 'scissors'
+        lose: 'scissors',
+        icon: paperIcon,
+        bg: 'radial-gradient(hsl(230, 89%, 62%), hsl(230, 89%, 65%))',
+        borderBottom: 'rgba(0, 0, 255, 0.5) 10px solid'
     }, {
         name: 'scissors',
         beat: 'paper',
-        lose: 'rock'
+        lose: 'rock',
+        icon: scissorsIcon,
+        bg: 'radial-gradient(hsl(39, 89%, 49%), hsl(40, 84%, 53%))',
+        borderBottom: 'rgba(187, 121, 0, 1) 10px solid'
     }];
 
-    const npcMove: string[] = ['rock', 'paper', 'scissors'];
 
-    const [party, setParty] = useState(true);
+    //User's hand choice
+    const [usersMove, setUsersMove] = useState<moves>({
+        name: '',
+        beat: '',
+        lose: '',
+        icon: '',
+        bg: '',
+        borderBottom: ''
+    });
 
-    const handleClick = (hand: moves) => {
-        setParty(!party);
+    //NPC's hand choice
+    const [npcsMove, setNpcsMove] = useState<moves>({
+        name: '',
+        beat: '',
+        lose: '',
+        icon: '',
+        bg: '',
+        borderBottom: ''
+    });
+
+    //Continue
+    const [again, setAgain] = useState(true);
+
+    //Summary text (Win, lose, or draw)
+    const [text, setText] = useState('');
+
+    const handleClick = (item: moves) => {
         let npcIndex: number = Math.floor(Math.random() * 3);
-        console.log('You choosed ' + hand.name + ' and the house picked ' + npcMove[npcIndex]);
-        if (hand.name == npcMove[npcIndex]) {
-            console.log('Draw');
-        }
-        else if (hand.beat == npcMove[npcIndex]) {
-            console.log('You won');
-        }
-        if (hand.lose == npcMove[npcIndex]) {
-            console.log('You lose');
-        }
-
+        setAgain(!again);
+        setUsersMove(item);
+        setNpcsMove(hand[npcIndex]);
     };
 
+    const handleLost = () => {
+        setAgain(!again);
+    };
+
+    useEffect(() => {
+
+
+        if (usersMove.name == npcsMove.name) {
+            console.log('Draw');
+            setText('Draw');
+        }
+        else if (usersMove.beat == npcsMove.name) {
+            console.log('You won');
+            setText('You won');
+        }
+        if (usersMove.lose == npcsMove.name) {
+            console.log('You lose');
+            setText('You lost');
+        }
+
+    }, [npcsMove])
+
+
     return (
-        <div className="game">
-            {party ? <div className="hand-selection">
+        <div className="game" style={{ backgroundImage: again ? `url(${triangle})` : 'none' }}>
+            {again ? <div className="hand-selection">
                 <div className="top-hand">
                     <div className="paper" onClick={() => handleClick(hand[1])}>
                         <div className="inner-paper"></div>
@@ -60,7 +113,25 @@ const Game = () => {
                 </div>
             </div> :
                 <div className="summary">
-
+                    <div className='outer-layer'>
+                        <div className='middle-layer'>
+                            <div className='inner-layer'>
+                                <div className="hand" style={{ background: `${usersMove.bg}`, borderBottom: `${usersMove.borderBottom}` }}>
+                                    <div className="inner-hand" style={{ backgroundImage: `url(${usersMove.icon})` }}></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <h1 onClick={handleLost}>{text}</h1>
+                    <div className='outer-layer'>
+                        <div className='middle-layer'>
+                            <div className='inner-layer'>
+                                <div className="hand" style={{ background: `${npcsMove.bg}`, borderBottom: `${npcsMove.borderBottom}` }}>
+                                    <div className="inner-hand" style={{ backgroundImage: `url(${npcsMove.icon})` }}></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             }
         </div>
